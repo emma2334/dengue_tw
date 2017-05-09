8aaa34887aca8da2058c4f05d1610cddc546daac
Emma <emma2334@gmail.com>
2017-05-09 17:54:19 +0800

    Center the map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-var svg = d3.select("svg#map");
+var map = d3.select("svg#map").node().getBoundingClientRect();
+var svg = d3.select("svg#map").attr('viewBox', `0 0 ${map.width} ${map.height}`);
 var projection = d3.geo.mercator()
   .center([121,24])
-  .scale(6000);
+  .scale(map.height/0.01/8.5)
+  .translate([map.width / 2, map.height / 2]);
 var path = d3.geo.path().projection(projection);
 
 var getCity = new Promise(function(resolve) {
@@ -26,10 +28,10 @@ var getTown = new Promise(function(resolve) {
 
       svg
         .append("g")
+        .attr("class", "city")
         .selectAll("path")
         .data(cityFeatures).enter()
         .append("path").attr({
-          class: "city",
           id: function(d) {
             return d.properties["C_Name"];
           },
@@ -38,12 +40,13 @@ var getTown = new Promise(function(resolve) {
 
       svg
         .append("g")
+        .attr("class", "town")
         .selectAll("path")
         .data(townFeatures).enter()
         .append("path")
         .attr({
           class: function(d) {
-            return `town ${d.properties["C_Name"]}`;
+            return d.properties["C_Name"];
           },
           id: function(d) {
             return d.properties["T_Name"];
