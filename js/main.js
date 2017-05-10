e2610cd58dcc557d32dd88b102b7ed14a7896664
Emma <emma2334@gmail.com>
2017-05-10 23:23:43 +0800

    Apply colors to map

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var getTown = new Promise(function(resolve) {
 
       svg
         .append("g")
-        .attr("class", "city")
+        .attr("class", "city active")
         .selectAll("path")
         .data(cityFeatures).enter()
         .append("path").attr({
@@ -55,3 +55,47 @@ var getTown = new Promise(function(resolve) {
         });
     });
 })();
+
+var rawData, fData;
+var start, end;
+var csv = d3.dsv(",", "text/csv;charset=big5");
+csv("https://nidss.cdc.gov.tw/Download/Age_County_Gender_061.csv", function(data){
+  rawData = data;
+  start = d3.min(data, function(d){ return d["發病年份"] });
+  end = d3.max(data, function(d){ return d["發病年份"] });
+  fData = yearFilter(end);
+  colorMap();
+});
+
+function yearFilter(year){
+  return rawData.filter(function(d){
+    return d["發病年份"] == year;
+  });
+}
+
+function colorMap(){
+  // city
+  var cityCount = d3.nest()
+    .key(function(d){ return d["縣市"] })
+    .rollup(function(d){
+      var count = 0;
+      for(var i=0; i<d.length; i++){
+        count += +d[i]["確定病例數"];
+      }
+      return count;
+    }).entries(fData);
+
+  var max = d3.max(cityCount, function(d){ return d.values });
+  var scale = d3.scale.linear().domain([0, max]).range([0, 20]);
+
+  var color = d3.scale
+    .linear()
+    .domain([0,10])
+    .range(["#E4E4E4", "#B50C0C"]);
+
+  for(var i=0; i<cityCount.length; i++){
+    d3.select("#" + cityCount[i].key).attr({
+      fill: color(scale(cityCount[i].values))
+    });
+  }
+}
