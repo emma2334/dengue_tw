8d766238f062ec948082eee5c5f54a913fc2171a
Emma <emma2334@gmail.com>
2017-05-10 23:50:22 +0800

    Apply colors to town map

--- js/main.js
+++ js/main.js
@@ -46,10 +46,7 @@ var getTown = new Promise(function(resolve) {
         .append("path")
         .attr({
           class: function(d) {
-            return d.properties["C_Name"];
-          },
-          id: function(d) {
-            return d.properties["T_Name"];
+            return `${d.properties["C_Name"]} ${d.properties["T_Name"]}`;
           },
           d: path
         });
@@ -74,6 +71,12 @@ function yearFilter(year){
 }
 
 function colorMap(){
+  // color scale
+  var color = d3.scale
+    .linear()
+    .domain([0,10])
+    .range(["#E4E4E4", "#B50C0C"]);
+
   // city
   var cityCount = d3.nest()
     .key(function(d){ return d["縣市"] })
@@ -88,14 +91,35 @@ function colorMap(){
   var max = d3.max(cityCount, function(d){ return d.values });
   var scale = d3.scale.linear().domain([0, max]).range([0, 20]);
 
-  var color = d3.scale
-    .linear()
-    .domain([0,10])
-    .range(["#E4E4E4", "#B50C0C"]);
-
   for(var i=0; i<cityCount.length; i++){
     d3.select("#" + cityCount[i].key).attr({
       fill: color(scale(cityCount[i].values))
     });
+  }
+
+  // town
+  var townCount = d3.nest()
+    .key(function(d){ return d["縣市"] })
+    .key(function(d){ return d["鄉鎮"] })
+    .rollup(function(d){
+      var count = 0;
+      for(var i=0; i<d.length; i++){
+        count += +d[i]["確定病例數"];
+      }
+      return count;
+    }).entries(fData);
+
+  max = d3.max(townCount, function(d){
+    return d3.max(d.values, function(c){ return c.values });
+  });
+  scale = d3.scale.linear().domain([0, max]).range([0, 20]);
+
+  for(var i=0; i<townCount.length; i++){
+    var tgt = townCount[i].values;
+    for(var j=0; j<tgt.length; j++){
+      d3.select(`.${townCount[i].key}.${tgt[j].key}`).attr({
+        fill: color(scale(tgt[j].values))
+      });
+    }
   }
 }
